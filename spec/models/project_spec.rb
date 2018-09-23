73186029fc92e2575cca28bdc7f02112333911a4
komoriyuichiro <thekomori1113@gmail.com>
2018-09-23 13:18:45 +0900

    leaned 03-models

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,74 @@
+require 'rails_helper'
+
+RSpec.describe Project, type: :model do
+  before do
+    @user = User.create(
+      first_name: "Joe",
+      last_name:  "Tester",
+      email:      "tester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
+  end
+
+  # 名前、ユーザーがあれば有効な状態であること
+  it "is valid with a user, project_name" do
+    project = Project.new(
+      name: "Test Project",
+      owner: @user,
+    )
+    expect(project).to be_valid
+  end
+
+  # 名前がなければ無効な状態であること
+  it "is invalid without a name" do
+    project = Project.new(
+      name: nil,
+      owner: @user,
+    )
+    project.valid?
+    expect(project.errors[:name]).to include("can't be blank")
+  end
+
+  # ユーザーがなければ無効な状態であること
+  it "is invalid without a user" do
+    project = Project.new(
+      name: "Test Project",
+      owner: nil,
+    )
+    expect(project).to_not be_valid
+  end
+
+  # ユーザー単位では重複したプロジェクト名を許可しないこと
+  it "does not allow duplicate project names per user" do
+    @user.projects.create(
+      name: "Test Project",
+    )
+
+    new_project = @user.projects.build(
+      name: "Test Project",
+    )
+
+    new_project.valid?
+    expect(new_project.errors[:name]).to include("has already been taken")
+  end
+
+  # 二人のユーザーが同じ名前を使うことは許可すること
+  it "allows two users to share a project name" do
+    @user.projects.create(
+      name: "Test Project",
+    )
+
+    other_user = User.create(
+      first_name: "Jane",
+      last_name:  "Tester",
+      email:      "janetester@example.com",
+      password:   "dottle-nouveau-pavilion-tights-furze",
+    )
+
+    other_project = other_user.projects.build(
+      name: "Test Project",
+    )
+
+    expect(other_project).to be_valid
+  end
+end
